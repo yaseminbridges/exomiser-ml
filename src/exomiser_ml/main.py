89b3455a80da165fd4fa211f96a1071b09d4b740
Yasemin Bridges <y.bridges@qmul.ac.uk>
2025-04-28 19:06:00 +0100

    remove methods

--- src/exomiser_ml/main.py
+++ src/exomiser_ml/main.py
@@ -4,7 +4,6 @@ from typing import List
 import click
 
 from exomiser_ml.data.create_features.add_features import add_features
-from exomiser_ml.data.extract_features.extract_features import extract_features
 from exomiser_ml.data.split_data.split_train_and_test import split_train_and_test
 from exomiser_ml.models.logistic_regression import logistic_regression
 from exomiser_ml.post_process.post_process import post_process_test_dir
@@ -16,16 +15,19 @@ def main():
 
 def run_logistic_regression_pipeline(phenopacket_dir: Path, result_dir: Path, output_dir: Path, features: List[str],
                                      test_size: float):
+    output_dir.joinpath("added_features").mkdir(parents=True, exist_ok=True)
     add_features(phenopacket_dir=phenopacket_dir, result_dir=result_dir,
-                 output_dir=output_dir.joinpath("results_added_features"))
-    extract_features(result_dir=output_dir.joinpath("results_added_features"), features=features,
-                     output_dir=output_dir.joinpath("results_extracted_features"))
-    split_train_and_test(input_dir=output_dir.joinpath("results_extracted_features"),
+                 output_dir=output_dir.joinpath("added_features"))
+    output_dir.joinpath("results_split/train").mkdir(parents=True, exist_ok=True)
+    output_dir.joinpath("results_split/test").mkdir(parents=True, exist_ok=True)
+    split_train_and_test(input_dir=output_dir.joinpath("added_features"),
                          output_dir=output_dir.joinpath("results_split"), test_size=test_size)
     logistic_regression(training_data=output_dir.joinpath("results_split/train/train.tsv"),
-                        test_dir=output_dir.joinpath("results_split/test"))
+                        test_dir=output_dir.joinpath("results_split/test"), features=features,)
+    output_dir.joinpath("pheval_variant_results").mkdir(parents=True, exist_ok=True)
     post_process_test_dir(test_dir=output_dir.joinpath("results_split/test"), phenopacket_dir=phenopacket_dir,
-                          output_dir=output_dir.joinpath("pheval_variant_results"))
+                          output_dir=output_dir)
+    output_dir.joinpath("added_features").unlink(missing_ok=True)
 
 
 @click.command("run-lr")
