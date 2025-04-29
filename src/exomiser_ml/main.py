9d66fc606ea2cd3f4220dbd2e3c6d1397a9611ba
Yasemin Bridges <y.bridges@qmul.ac.uk>
2025-04-29 22:09:04 +0100

    add post process command

--- src/exomiser_ml/main.py
+++ src/exomiser_ml/main.py
@@ -3,6 +3,7 @@ import click
 from exomiser_ml.data.create_features.add_features import add_features_command
 from exomiser_ml.data.split_data.split_train_and_test import split_data
 from exomiser_ml.models.logistic_regression import run_logistic_regression, run_lr_pipeline
+from exomiser_ml.post_process.post_process import post_process_test_dir_command
 
 
 @click.group()
@@ -19,6 +20,7 @@ ml.add_command(run_lr_pipeline)
 ml.add_command(add_features_command)
 ml.add_command(split_data)
 ml.add_command(run_logistic_regression)
+ml.add_command(post_process_test_dir_command)
 
 if __name__ == "__main__":
     main()
