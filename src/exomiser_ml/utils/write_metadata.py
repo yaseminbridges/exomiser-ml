e63e066bb89b2f6460dd7970a6c021e63f19f3d1
Yasemin Bridges <y.bridges@qmul.ac.uk>
2025-04-29 13:44:19 +0100

    add fields

--- src/exomiser_ml/utils/write_metadata.py
+++ src/exomiser_ml/utils/write_metadata.py
@@ -1,23 +1,27 @@
-from pydantic import BaseModel
+from dataclasses import dataclass
+
 from pathlib import Path
-from typing import List, Optional
+from typing import List, Union
 from datetime import datetime
 import yaml
+from serde import to_dict, serde
 
-class RunMetadata(BaseModel):
-    test_size: float
+
+@serde
+@dataclass
+class RunMetadata:
+    test_size: Union[float, None]
     output_dir: Path
     model_type: str
+    training_data: Path
+    test_dir: Path
     features_used: List[str]
     timestamp: datetime = datetime.now()
 
-    class Config:
-        arbitrary_types_allowed = True  # allow Path types
+
 
 def write_metadata_yaml(metadata: RunMetadata, output_path: Path) -> None:
     output_path = output_path / "run_metadata.yaml"
-
-    metadata_dict = metadata.dict()
-
-    with output_path.open("w") as f:
-        yaml.safe_dump(metadata_dict, f, sort_keys=False)
+    with open(output_path, "w") as metadata_file:
+        yaml.dump(to_dict(metadata), metadata_file, sort_keys=False, default_style="")
+    metadata_file.close()
