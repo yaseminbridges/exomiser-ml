949c34b4f6a1986eefe50fdeb617c6d60feb5793
Yasemin Bridges <y.bridges@qmul.ac.uk>
2025-07-04 10:23:21 +0100

    add command

--- src/exomiser_ml/main.py
+++ src/exomiser_ml/main.py
@@ -1,7 +1,7 @@
 import click
 
 from exomiser_ml.cli import run_model_command, run_model_pipeline, add_features_command, split_data_command, \
-    post_process_test_dir_command
+    post_process_test_dir_command, run_manual_logistic_regression_model_command
 
 
 @click.group()
@@ -19,6 +19,7 @@ ml.add_command(split_data_command)
 ml.add_command(post_process_test_dir_command)
 ml.add_command(run_model_command)
 ml.add_command(run_model_pipeline)
+ml.add_command(run_manual_logistic_regression_model_command)
 
 if __name__ == "__main__":
     main()
