93b98c91ec1ecd45281ea0e9a7e73fc98c3accfc
Yasemin Bridges <y.bridges@qmul.ac.uk>
2025-12-23 12:06:36 +0000

    deal with compound het variants

--- src/exomiser_ml/data/create_features/add_features.py
+++ src/exomiser_ml/data/create_features/add_features.py
@@ -2,12 +2,24 @@ from pathlib import Path
 
 import polars as pl
 from pheval.utils.file_utils import all_files
+from enum import Enum
 
 from exomiser_ml.data.create_features.calculate_acmg_ppp import ACMGPPPCalculator
 from exomiser_ml.data.create_features.get_causative_variant import extract_causative_variants
 
 EXOMISER_TSV_FILE_SUFFIX = "-exomiser.variants.tsv"
 
+class ModeOfInheritance(Enum):
+    AUTOSOMAL_DOMINANT = 1
+    AD = 1
+    AUTOSOMAL_RECESSIVE = 2
+    AR = 2
+    X_DOMINANT = 1
+    XD = 1
+    X_RECESSIVE = 2
+    XR = 2
+    MITOCHONDRIAL = 3
+    MT = 3
 
 def get_result(phenopacket_path: Path, result_dir: Path) -> pl.DataFrame:
     result_path = result_dir.joinpath(phenopacket_path.stem + EXOMISER_TSV_FILE_SUFFIX)
@@ -41,14 +53,27 @@ def add_features(phenopacket_dir: Path, result_dir: Path, output_dir: Path, filt
             ).alias("ACMG_PPP")
         ])
         acmg_ppp = acmg_ppp.with_columns(
-            (pl.col("EXOMISER_ACMG_EVIDENCE") == "").alias("ACMG_PPP_NULL")
+            pl.col("MOI")
+            .map_elements(
+                lambda moi: ModeOfInheritance[moi].value,
+                return_dtype=pl.Int8,
+            )
+            .alias("moi_enum")
+        ).with_columns(
+            (pl.col("moi_enum") == 2).alias("is_recessive")
         )
-        mean_val = acmg_ppp.filter(~pl.col("ACMG_PPP_NULL"))["ACMG_PPP"].mean()
-        print(f"Mean value for ACMG_PPP is: {mean_val}")
         acmg_ppp = acmg_ppp.with_columns(
-            pl.when(pl.col("ACMG_PPP_NULL"))
-            .then(mean_val)
+            pl.when(
+                (pl.col("is_recessive"))
+                & (pl.col("CONTRIBUTING_VARIANT").cast(pl.Int64) == 1)  # noqa: E712
+            )
+            .then(
+                pl.col("ACMG_PPP")
+                .mean()
+                .over("GENE_SYMBOL")
+            )
             .otherwise(pl.col("ACMG_PPP"))
-            .alias("ACMG_PPP")
+            .alias("ACMG_PPP_AVE")
         )
+
         acmg_ppp.write_csv(output_dir.joinpath(phenopacket_path.stem + EXOMISER_TSV_FILE_SUFFIX), separator="\t")
--- src/exomiser_ml/post_process/post_process.py
+++ src/exomiser_ml/post_process/post_process.py
@@ -1,21 +1,92 @@
 from pathlib import Path
-
+import uuid
 from pheval.post_processing.post_processing import generate_variant_result, SortOrder
 from pheval.utils.file_utils import all_files
 import polars as pl
+from enum import Enum
 
+class ModeOfInheritance(Enum):
+    AUTOSOMAL_DOMINANT = 1
+    AD = 1
+    AUTOSOMAL_RECESSIVE = 2
+    AR = 2
+    X_DOMINANT = 1
+    XD = 1
+    X_RECESSIVE = 2
+    XR = 2
+    MITOCHONDRIAL = 3
+    MT = 3
+
+def extract_variant_result(exomiser_result: pl.DataFrame, score_name) -> pl.DataFrame:
+    contributing_variant_only = exomiser_result.filter(
+        pl.col("CONTRIBUTING_VARIANT").cast(pl.Int64) == 1  # noqa
+    )
+    return (
+        contributing_variant_only.select(
+            [
+                pl.col("GENE_SYMBOL"),
+                pl.col("CONTIG").alias("chrom").cast(pl.String),
+                pl.col("START").cast(pl.Int64).alias("start"),
+                pl.col("END").cast(pl.Int64).alias("end"),
+                pl.col("REF").alias("ref"),
+                pl.col("ALT").alias("alt"),
+                pl.col(score_name).alias("score"),
+                pl.col("MOI")
+                .map_elements(lambda moi: ModeOfInheritance[moi].value, return_dtype=pl.Int8)
+                .alias("moi_enum"),
+            ]
+        )
+        .with_columns(
+            [
+                (pl.col("moi_enum") == 2).alias("is_recessive"),
+                pl.when(pl.col("moi_enum") == 2)
+                .then(
+                    pl.format(
+                        "recessive|{}|{}|{}",
+                        pl.col("GENE_SYMBOL"),
+                        pl.col("score"),
+                        pl.col("moi_enum"),
+                    )
+                )
+                .otherwise(
+                    pl.format(
+                        "dominant|{}|{}|{}|{}|{}|{}",
+                        pl.col("chrom"),
+                        pl.col("start"),
+                        pl.col("end"),
+                        pl.col("ref"),
+                        pl.col("alt"),
+                        pl.col("score"),
+                    )
+                )
+                .alias("group_key"),
+            ]
+        )
+        .with_columns(
+            [
+                pl.col("group_key")
+                .rank("dense")
+                .cast(pl.UInt32)
+                .map_elements(
+                    lambda i: str(uuid.uuid5(uuid.NAMESPACE_DNS, str(i))), return_dtype=pl.String
+                )
+                .alias("grouping_id")
+            ]
+        )
+    )
 
 def post_process_test_dir(test_dir: Path, phenopacket_dir: Path, output_dir: Path, score: str = "NEW_SCORE") -> None:
     for output_file in all_files(test_dir):
         output = pl.read_csv(output_file, separator="\t", infer_schema_length=10000)
-        variant_results = output.select([
-            pl.col("CONTIG").alias("chrom").cast(pl.String),
-            pl.col("START").alias("start").cast(pl.Int64),
-            pl.col("END").alias("end").cast(pl.Int64),
-            pl.col("REF").alias("ref").cast(pl.String),
-            pl.col("ALT").alias("alt").cast(pl.String),
-            pl.col(score).alias("score").cast(pl.Float64),
-        ])
+        variant_results = extract_variant_result(output, score)
+        # variant_results = output.select([
+        #     pl.col("CONTIG").alias("chrom").cast(pl.String),
+        #     pl.col("START").alias("start").cast(pl.Int64),
+        #     pl.col("END").alias("end").cast(pl.Int64),
+        #     pl.col("REF").alias("ref").cast(pl.String),
+        #     pl.col("ALT").alias("alt").cast(pl.String),
+        #     pl.col(score).alias("score").cast(pl.Float64),
+        # ])
         generate_variant_result(results=variant_results,
                                 phenopacket_dir=phenopacket_dir,
                                 sort_order=SortOrder.DESCENDING,
